8d80fc6f9838c46d1c79f300ee4e5ff6eefac2f5
iamwilsky <willyarsal@gmail.com>
2025-12-23 00:45:32 +0700

    Implement favicons: add manifest.ts and separate icons to public folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,23 @@ export const metadata: Metadata = {
         index: true,
         follow: true,
     },
+    icons: {
+        icon: [
+            { url: '/favicon.ico' },
+            { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+            { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+        ],
+        apple: [
+            { url: '/apple-touch-icon.png' },
+        ],
+        other: [
+            {
+                rel: 'apple-touch-icon-precomposed',
+                url: '/apple-touch-icon.png',
+            },
+        ],
+    },
+    manifest: '/manifest.json',
 }
 
 export default function RootLayout({
